da4b27b24400b2ce79df8cc9cb237184ca49beec
Orca19 <yarinm1910@gmail.com>
2023-10-17 12:08:31 +0300

    AnswerB

--- Answers.py
+++ Answers.py
@@ -1,18 +1,24 @@
 class Converter:
-    def __init__(self, voltage):
+    def __init__(self, voltage, maxV, bits):
         self.voltage = voltage
+        self.Max_Voltage = maxV
+        self.bits = bits
+    def Difference(self):
+        dif = self.Max_Voltage/((2**self.bits)-1)
+        return dif
+    
     def ToDigital(self):
-        dif = 5/1024
-        digital = bin(int((self.voltage/dif)-1))
+        dif = self.Difference()
+        digital = bin(int((self.voltage/dif)))
         return digital[2:]
     
     def SetAnalogValue(self, digital):
-        dif = 5/1024
+        dif = self.Difference()
         voltage = (int(digital)+1) * dif
         self.voltage  = voltage
         print(f'Voltage updated to {self.voltage} volt')
 
-con  = Converter(3)
+con  = Converter(5,5,10)
 print(con.ToDigital())
 print(con.SetAnalogValue(0b1111111110))
 
