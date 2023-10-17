d8b3c4ef76431ca699c595c517caa2705404f35a
Orca19 <yarinm1910@gmail.com>
2023-10-17 12:54:41 +0300

    updated inputs

--- Answers.py
+++ Answers.py
@@ -14,11 +14,15 @@ class Converter:
     
     def SetAnalogValue(self, digital):
         dif = self.Difference()
-        voltage = (int(digital)+1) * dif
+        voltage = (int(digital)) * dif
         self.voltage  = voltage
         print(f'Voltage updated to {self.voltage} volt')
 
-con  = Converter(5,5,10)
-print(con.ToDigital())
-print(con.SetAnalogValue(0b1111111110))
+cur_Voltage = float(input("Enter the current voltage "))
+Max_Voltage = float(input("Enter the max voltage "))
+bits = float(input("Enter the number of bits "))
+con  = Converter(cur_Voltage,Max_Voltage,bits)
+print("The current digital number is ",con.ToDigital())
+digital = int(input("Enter current digital number in binary base "), base=2)
+con.SetAnalogValue(digital)
 
